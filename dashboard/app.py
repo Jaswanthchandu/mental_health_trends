31b54b80a5e79d46928c97699ae738466d97f474
Jaswanthchandu <jaswanthchandu5@gmail.com>
2026-04-17 18:26:06 -0400

    Fixed file path for Render

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,8 +6,10 @@ import plotly.graph_objects as go
 import pandas as pd
 
 # Load data
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 file_path = os.path.join(BASE_DIR, "data", "processed", "mental_health_clean.csv")
+
+print("DEBUG PATH:", file_path)  # optional debug
 
 df = pd.read_csv(file_path)
 
